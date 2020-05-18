6f7815ca748af39661fc404c573b0b84a38ea1a4
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-05-17 21:56:59 -0300

    feat: todo item component
    Adds the Todo Item component to be used in the list.

---
+++ components/TodoItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TodoItem = ({ todo }) => {
+  const { title } = todo;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.todo}>{title}</Text>
+    </View>
+  );
+};
+
+export default TodoItem;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#e1e1e1',
+    marginTop: 20,
+  },
+  todo: {
+    alignItems: 'flex-start',
+    fontSize: 14,
+  },
+});
